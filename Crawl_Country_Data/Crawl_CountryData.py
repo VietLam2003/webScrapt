7b2cd687799f2b97a3c8e65396685dd1a17f4989
vietlam <vietlam050607@gmail.com>
2023-10-31 21:48:39 +0700

    this is commit from vietla

---
+++ Crawl_Country_Data/Crawl_CountryData.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+import io
+import os
+from shutil import copyfile
+import pandas as pd
+import plotly.express as px
+
+
+url = 'https://www.worldometers.info/world-population/population-by-country/'
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text,'html.parser')
+
+rows = soup.find('table',{'id':'example2'}).find('tbody').find_all('tr')
+
+
+countries_list = []
+for row in rows:
+   dic = {} 
+   dic['#'] = row.find_all('td')[0].text
+   dic['Country'] = row.find_all('td')[1].text # to extract the country name
+   dic['Population 2023'] = row.find_all('td')[2].text.replace(',','')
+   dic['Land Area(km^2)'] = row.find_all('td')[6].text.replace(',','')
+   dic['Migrants(net)'] = row.find_all('td')[7].text.replace(',','')
+  
+  
+   countries_list.append(dic)
+
+df = pd.DataFrame(countries_list)
+
+df.to_csv('countries_data.csv', index=False) # to save the data in csv file
+pd.read_csv('countries_data.csv')
+# df.to_excel('countries_data.xlsx') # to save the data in the excel
+
+df_plot = df.head(10)
+
+# extract the country_name and population column
+country_name = df_plot['Country']
+population = df_plot['Population 2023'] 
+
+fig = px.bar(x=country_name, y=population)
+fig.update_layout(title="Population in 2023 by Country")
+fig.update_xaxes(title_text="Country")
+fig.update_yaxes(title_text="Population 2023")
+
+print(fig.show())
